5dfe281f0bf25e05267bf64e64744aabe74de4ae
Mattias.Konkov <mattias.konkov@guardtime.com>
2017-02-09 13:46:20 +0200

    Integration tests added.

--- test/test.sh
+++ test/test.sh
@@ -19,27 +19,18 @@
 # reserves and retains all trademark rights.
 
 # Remove test output directories.  
+rm -rf test/out/integrate 2> /dev/null
 rm -rf test/out/sign 2> /dev/null
 rm -rf test/out/extend 2> /dev/null
-rm -rf test/out/pubfile 2> /dev/null
-rm -rf test/out/fname 2> /dev/null
 
 # Create test output directories.
+mkdir -p test/out/integrate
 mkdir -p test/out/sign
 mkdir -p test/out/extend
-mkdir -p test/out/pubfile
-mkdir -p test/out/fname
 
 # Create some test files to output directory.
-cp test/resource/file/testFile	test/out/fname/_
-cp test/resource/file/testFile	test/out/fname/10__
-cp test/resource/file/testFile	test/out/fname/test_file
-cp test/resource/file/testFile	test/out/fname/a_23_500
-cp test/resource/file/testFile	test/out/fname/a_23_1000
-cp test/resource/file/testFile	test/out/fname/a_23_1000.ksig
-cp test/resource/file/testFile	test/out/fname/a_23_1000_5.ksig
-cp test/resource/signature/ok-sig-2014-08-01.1.ksig test/out/fname/ok-sig.ksig
-cp test/resource/signature/ok-sig-2014-08-01.1.ksig test/out/fname/ok-sig
+cp -r test/resource/signature/syslog.logsig.parts test/out/integrate/syslog.logsig.parts
+cp test/resource/file/syslog test/out/integrate/syslog
 
 # Define KSI_CONF for temporary testing.
 export KSI_CONF=test/resource/conf/default-not-working-conf.cfg
@@ -61,29 +52,9 @@ else
 fi
 
 shelltest \
-test/test_suites/sign.test \
-test/test_suites/static-sign.test \
-test/test_suites/sign-verify.test \
-test/test_suites/extend.test \
-test/test_suites/extend-verify.test \
-test/test_suites/static-verify.test \
-test/test_suites/static-sign-verify.test \
-test/test_suites/static-extend.test \
-test/test_suites/linux-pipe.test \
-test/test_suites/sign-cmd.test \
-test/test_suites/extend-cmd.test \
-test/test_suites/static-verify-invalid-signatures.test \
-test/test_suites/pubfile.test \
-test/test_suites/static-pubfile.test \
-test/test_suites/verify-invalid-pubfile.test \
-test/test_suites/verify-cmd.test \
-test/test_suites/default-conf.test \
-test/test_suites/invalid-conf.test \
-test/test_suites/file-name-gen.test \
-test/test_suites/sign-block-signer.test \
-test/test_suites/sign-block-signer-cmd.test \
-$TEST_DEPENDING_ON_TLVUTIL \
+test/test_suites/integrate.test \
 --with=$tool -- -j1
+
 exit_code=$?
 
 exit $exit_code
