81fb2a5c6b2ce4167f9b6953314fd39fe479f592
Mattias.Konkov <mattias.konkov@guardtime.com>
2017-02-13 14:58:58 +0200

    Removed gttlvutil tests.

--- test/test.sh
+++ test/test.sh
@@ -40,14 +40,6 @@ else
 	tool=src/logksi
 fi
 
-if gttlvdump -h > /dev/null && gttlvgrep -h > /dev/null; then
-	TEST_DEPENDING_ON_TLVUTIL="test/test_suites/sign-metadata.test test/test_suites/sign-masking.test"
-	echo Info: Extra tests depending on gttlvutil added.
-else
-	TEST_DEPENDING_ON_TLVUTIL=""
-	echo Warning: gttlvutil is not installed. Tests depending on gttlvutil are ignored.
-fi
-
 shelltest \
 test/test_suites/integrate.test \
 test/test_suites/sign.test \
